bfee3e32d393f97d1ad22df52cd1eeaf8c500c62
Robert Jackson <robert.w.jackson@me.com>
2015-03-30 22:27:41 -0400

    Add ember-try.

---
+++ config/ember-try.js
@@ -0,0 +1,28 @@
+module.exports = {
+  scenarios: [
+    {
+      name: 'ember-1.10',
+      dependencies: {
+        "ember": "1.10.0"
+      }
+    },
+    {
+      name: 'ember-1.11.0',
+      dependencies: {
+        "ember": "1.11.0"
+      }
+    },
+    {
+      name: 'ember-1.12.0-beta.1',
+      dependencies: {
+        "ember": "1.12.0-beta.1"
+      }
+    },
+    //{
+    //  name: 'ember-canary',
+    //  dependencies: {
+    //    "ember": "components/ember#canary"
+    //  }
+    //}
+  ]
+};
